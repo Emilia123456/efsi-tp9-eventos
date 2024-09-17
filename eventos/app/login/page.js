a3545d27762131543f058d2c324df3f62987e999
Android Studio <devandroid@ort>
2024-09-17 09:00:55 -0300

    no sabemos si conectó con la api

--- eventos/app/login/page.js
+++ eventos/app/login/page.js
@@ -1,15 +1,29 @@
 "use client"; 
 import styles from './Login.module.css';
 import { useRouter } from 'next/navigation'; 
+import react from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import Form from '../components/Form/Form';
+import { useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const router = useRouter();
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    router.push('/events');
+  const handleLogin = async () => {
+    try {
+      const result = await login(email, password);
+
+      if (result.success) {  
+        router.push('/events');
+      } else {
+        window.alert("Error", "Hubo un problema al iniciar sesión. Inténtalo de nuevo más tarde.");   
+      }
+
+    } catch (error) {
+      window.alert("Error", "Hubo un problema al iniciar sesión. Inténtalo de nuevo más tarde.");
+    }
   };
 
   return (
@@ -19,9 +33,9 @@ export default function Login() {
         title="¡Hola de nuevo!" 
         buttonText="Iniciar sesión"
         fields={[
-          { label: 'Email', type: 'email', placeholder: 'Email' },
-          { label: 'Contraseña', type: 'password', placeholder: 'Password' },
-        ]}
+          { label: 'Email', type: 'email', placeholder: 'Email', value: email },
+          { label: 'Contraseña', type: 'password', placeholder: 'Password', value: password },
+        ]}        
         onSubmit={handleLogin}
       />
       </div>
---
+++ eventos/app/services/login-service.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+const userApi = axios.create({
+  baseURL: 'http://localhost:3001/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const login = async (email, password) => {
+  try {
+    const response = await userApi.post('/user/login', {
+      email: email,
+      password: password, 
+    });
+
+    console.log('LLEGO ACA');
+    console.log(email, password);
+    console.log(response.data);
+
+    return response.data;  
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    console.error(error);
+    throw error; 
+  }
+};
+/* 
+
+export const register = async (email, nombre, apellido, direccion, password, genero, foto, fecha) => {
+  console.log("hola")
+  try {
+    const response = await userApi.post('/users/register', {
+      email: email, 
+      nombre: nombre,
+      apellido: apellido, 
+      direccion: direccion, 
+      contrasena: password,
+      idGenero: genero, 
+      foto: foto, 
+      FechaNacimiento: fecha
+
+
+    });
+    //if (response.satatu = 201)
+
+    return response.data;  
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    console.error(error);
+    throw error;  
+  }
+}; */
